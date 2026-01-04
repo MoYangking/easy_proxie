140799bef246901aa64e48b69877b90712cdd01f
moyang <3034065341@qq.com>
2026-01-05 02:01:24 +0800

    xxx

---
+++ cmd/easy_proxies/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"easy_proxies/internal/app"
+	"easy_proxies/internal/config"
+)
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	if created, err := config.EnsureConfigFile(configPath); err != nil {
+		log.Fatalf("ensure config: %v", err)
+	} else if created {
+		log.Printf("config not found, created default config at %s", configPath)
+	}
+
+	cfg, err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("load config: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if err := app.Run(ctx, cfg); err != nil {
+		fmt.Fprintf(os.Stderr, "proxy pool exited with error: %v\n", err)
+		os.Exit(1)
+	}
+}
